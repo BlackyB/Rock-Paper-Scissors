03b846f4bab8aec44b680146d06562010692937a
Bari Jonathan <bari.jonathan@live.fr>
2019-07-15 13:42:20 +0200

    Remove active

--- script/script.js
+++ script/script.js
@@ -644,12 +644,11 @@ let initPotatoe = () =>{
 	clean();
 	up.removeEventListener('click', moveUp3);
 	down.removeEventListener('click', moveDown3);
-	displayTop.innerHTML = "00:00";
+	displayTop.innerHTML = "30:00";
 	display2.innerHTML = "PASS IT";
 	for (var i = 0; i < displays.length; i++) {
 		displays[i].classList.remove('displayActive');
 	}
-	display2.classList.add('displayActive');
 	display2.backgroundColor = "#9B9F4E";
 	display2.color = "rgb(10, 10, 10)";
 	display1.style.backgroundColor = "rgb(10, 10, 10)";
